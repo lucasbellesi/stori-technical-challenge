82a0dc93ffe4e2fdbdb1bda71b7ebb8008297042
lucasbellesi <lucasbellesi@lucasbellesis-MacBook-Air.local>
2024-06-14 13:12:30 -0300

    added end to end test

---
+++ tests/e2e_test.go
@@ -0,0 +1,81 @@
+package tests
+
+import (
+	"os"
+	"stori-technical-challenge/config"
+	"stori-technical-challenge/pkg/db"
+	"stori-technical-challenge/pkg/transactions"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func createTestCSV(filePath string) {
+	file, err := os.Create(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	file.WriteString("Id,Date,Transaction\n")
+	file.WriteString("0,7/15,+60.5\n")
+	file.WriteString("1,7/28,-10.3\n")
+	file.WriteString("2,8/2,-20.46\n")
+	file.WriteString("3,8/13,+10\n")
+	file.WriteString("\n")
+}
+
+func TestE2E(t *testing.T) {
+	// Configurar variables de entorno directamente
+	os.Setenv("SMTP_HOST", "localhost")
+	os.Setenv("SMTP_PORT", "1025") // Puerto por defecto de MailHog
+	os.Setenv("SMTP_USER", "")
+	os.Setenv("SMTP_PASSWORD", "")
+	os.Setenv("FROM_EMAIL", "test@example.com")
+
+	// Cargar la configuración sin depender del archivo .env
+	config.AppConfig = config.Config{
+		SMTPHost:     os.Getenv("SMTP_HOST"),
+		SMTPPort:     1025, // Convertir el puerto de cadena a entero
+		SMTPUser:     os.Getenv("SMTP_USER"),
+		SMTPPassword: os.Getenv("SMTP_PASSWORD"),
+		FromEmail:    os.Getenv("FROM_EMAIL"),
+	}
+
+	err := db.InitDB()
+	assert.NoError(t, err, "Error initializing database")
+
+	filePath := "test_transactions.csv"
+	createTestCSV(filePath)
+	defer os.Remove(filePath)
+
+	_, summary, err := transactions.ProcessTransactions(filePath)
+	assert.NoError(t, err, "Error processing transactions")
+
+	for month, data := range summary {
+		for _, amount := range data {
+			date := month + "-01" // Placeholder date
+			transaction := db.Transaction{
+				Date:   date,
+				Amount: amount,
+			}
+			err = db.SaveTransaction(transaction)
+			assert.NoError(t, err, "Error saving transaction")
+		}
+	}
+
+	// Omitir el envío de correo
+	// subject := "Transaction Summary"
+	// body := fmt.Sprintf("...") // Generar cuerpo del correo
+	// err = emailSender.SendEmail(subject, body, "recipient@example.com")
+	// assert.NoError(t, err, "Error sending email")
+
+	// Verificar que las transacciones se guardaron en la base de datos
+	transactions, err := db.GetAllTransactions()
+	assert.NoError(t, err, "Error retrieving transactions")
+	assert.NotEmpty(t, transactions, "No transactions found")
+
+	lastTransaction := transactions[len(transactions)-1]
+	assert.Equal(t, "2024-08-01", lastTransaction.Date, "Last transaction date does not match")
+	assert.Equal(t, 10.0, lastTransaction.Amount, "Last transaction amount does not match")
+}
