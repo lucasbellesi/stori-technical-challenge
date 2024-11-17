0ef1e194a8e484dc06a40bd304b9938e1660a5fc
lucasbellesi <lucasbellesi@MacBook-Air-de-lucasbellesi.local>
2024-11-17 20:16:36 -0300

    refactor lambda version handler file

--- lambda-version/handler.go
+++ lambda-version/handler.go
@@ -16,33 +16,42 @@ type Request struct {
 	S3Event events.S3EventRecord `json:"s3Event"`
 }
 
-func handleRequest(ctx context.Context, req Request) {
-	bucket := req.S3Event.S3.Bucket.Name
-	key := req.S3Event.S3.Object.Key
-
-	fmt.Printf("Processing file: s3://%s/%s\n", bucket, key)
-
-	totalBalance, summary, avgDebit, avgCredit, err := transactions.ProcessTransactionsFromS3(bucket, key)
-	if err != nil {
-		log.Fatalf("Error processing transactions: %v", err)
-		return
+func handleRequest(ctx context.Context, req Request) (string, error) {
+	// Validate input
+	if req.ToEmail == "" {
+		return "", fmt.Errorf("missing 'ToEmail' field")
 	}
 
+	if req.S3Event.S3.Bucket.Name == "" || req.S3Event.S3.Object.Key == "" {
+		return "", fmt.Errorf("invalid S3 event: bucket or key is missing")
+	}
+
+	bucket := req.S3Event.S3.Bucket.Name
+	key := req.S3Event.S3.Object.Key
+	log.Printf("Processing file: s3://%s/%s\n", bucket, key)
+
+	// Process transactions from S3
+	totalBalance, summary, avgDebit, avgCredit, err := transactions.ProcessTransactionsFromS3(bucket, key)
+	if err != nil {
+		return "", fmt.Errorf("error processing transactions: %w", err)
+	}
+
+	// Generate email content
 	emailData := email.GenerateEmailData(totalBalance, summary, avgDebit, avgCredit)
 	body, err := email.LoadTemplate("email_template.html", emailData)
 	if err != nil {
-		log.Fatalf("Error loading email template: %v", err)
-		return
+		return "", fmt.Errorf("error loading email template: %w", err)
 	}
 
+	// Send email
 	emailSender := email.SMTPSender{}
-	err = emailSender.SendEmail("Transaction Summary", body, req.ToEmail)
+	err = emailSender.SendEmail("Stori - Transaction Summary", body, req.ToEmail)
 	if err != nil {
-		log.Fatalf("Error sending email: %v", err)
-		return
+		return "", fmt.Errorf("error sending email: %w", err)
 	}
 
-	log.Println("Email sent successfully!")
+	log.Println("Email sent successfully")
+	return "Success", nil
 }
 
 func main() {
